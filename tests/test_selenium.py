6bb56e515e257324b10b8d7cce0b784bc05c9c51
BaneleMlamleli <mlamlelibanele@yahoo.com>
2023-08-05 01:48:06 +0200

    Added behave package

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import *
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://the-internet.kineticskunk.co.za/")
+
+driver.find_element(By.XPATH, "//a[normalize-space()='A/B Testing']").click()
+
+time.sleep(2)
+
+print('test run done')
